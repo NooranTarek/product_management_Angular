5ba4864a04a006bc5feb4dc9d20d90b3947f403a
NooranTarek <noort.mohamed@gmail.com>
2024-08-23 14:37:34 +0300

    adding product and category interfaces

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: number;
+    title: string;
+    category_id: number;
+    price: number;
+    description?: string;
+    image: string;
+    code: string;
+    created_at?: string;
+    updated_at?: string;
+}
